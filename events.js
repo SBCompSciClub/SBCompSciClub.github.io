d3e04d468784b39ec0179e8a20ee39b02d5c7478
Shivan Modha <shivanmodha@gmail.com>
2017-10-18 19:45:25 -0400

    Harsh Reload Test

--- events.js
+++ events.js
@@ -1,12 +1,1 @@
-this.addEventListener('activate', function (event)
-{
-    event.waitUntil(
-        caches.keys().then(function (keyList)
-        {
-            return Promise.all(keyList.map(function (key)
-            {
-                return caches.delete(key);
-            }));
-        })
-    );
-});
+window.location.reload(true);
