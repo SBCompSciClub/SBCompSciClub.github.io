7c18a562d4417dce603919abc692692b96b52f5c
Shivan Modha <shivanmodha@gmail.com>
2017-10-18 19:05:51 -0400

    Clearing Cache

--- events.js
+++ events.js
@@ -1,30 +1,3 @@
-// Check if a new cache is available on page load.
-window.addEventListener('load', function(e) {
-    
-      window.applicationCache.addEventListener('updateready', function(e) {
-        if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-          // Browser downloaded a new app cache.
-          // Swap it in and reload the page to get the new hotness.
-          window.applicationCache.swapCache();
-          if (confirm('A new version of this site is available. Load it?')) {
-            window.location.reload();
-          }
-        } else {
-          // Manifest didn't changed. Nothing new to server.
-        }
-      }, false);
-    
-    }, false);
-if ('serviceWorker' in navigator)
-{
-    navigator.serviceWorker.register('service-worker.js', {scope: 'sw-test'}).then(function(registration) {
-      // registration worked
-      console.log('Registration succeeded.');
-      registration.unregister().then(function(boolean) {
-          alert(boolean);
-      });
-    }).catch(function(error) {
-      // registration failed
-      console.log('Registration failed with ' + error);
-    });
-  };
+window.applicationCache.update();
+window.applicationCache.swapCache();
+window.location.reload();
