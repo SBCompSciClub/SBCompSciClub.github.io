952f7a34a81a989f00397a812463b9a4fe7ab60b
Shivan Modha <shivanmodha@gmail.com>
2017-10-18 19:29:52 -0400

    WE HATE CACHE

--- events.js
+++ events.js
@@ -1,15 +1,12 @@
-self.addEventListener('activate', function(event) {
+this.addEventListener('activate', function (event)
+{
     event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.filter(function(cacheName) {
-            // Return true if you want to remove this cache,
-            // but remember that caches are shared across
-            // the whole origin
-          }).map(function(cacheName) {
-            return caches.delete(cacheName);
-          })
-        );
-      })
+        caches.keys().then(function (keyList)
+        {
+            return Promise.all(keyList.map(function (key)
+            {
+                return caches.delete(key);
+            }));
+        })
     );
-  });
+});
