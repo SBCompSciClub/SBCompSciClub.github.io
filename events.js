58b80c145c2faa0ee536b24d19443c31a479c58e
Shivan Modha <shivanmodha@gmail.com>
2017-10-18 18:49:42 -0400

    New Service Worker

--- events.js
+++ events.js
@@ -1,1 +1,12 @@
-alert("testing");
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js', {scope: 'sw-test'}).then(function(registration) {
+      // registration worked
+      console.log('Registration succeeded.');
+      registration.unregister().then(function(boolean) {
+          alert(boolean);
+      });
+    }).catch(function(error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+  };
