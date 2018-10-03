f4d56fa974e0daa0a0907ec58cd1dc8a515e04eb
Rishav Bhagat <rishavb123@gmail.com>
2018-10-03 19:54:21 -0400

    got leader board

--- club/pages/home/demos/pong/leaderboard/app.js
+++ club/pages/home/demos/pong/leaderboard/app.js
@@ -1,56 +1,24 @@
 let config = {
-  apiKey: "AIzaSyCAlMrDCczQWmFYgZn0b6JIDSIwCUOANZc",
-  authDomain: "pfm-simulation.firebaseapp.com",
-  databaseURL: "https://pfm-simulation.firebaseio.com",
-  projectId: "pfm-simulation",
-  storageBucket: "pfm-simulation.appspot.com",
-  messagingSenderId: "562600285763"
+    apiKey: "AIzaSyCHpDKehgESGNKBQbFTJ3XOMea9vPZlV9I",
+    authDomain: "pong-demo-csc.firebaseapp.com",
+    databaseURL: "https://pong-demo-csc.firebaseio.com",
+    projectId: "pong-demo-csc",
+    storageBucket: "pong-demo-csc.appspot.com",
+    messagingSenderId: "847028439876"
 };
 firebase.initializeApp(config);
-let ref = firebase.database().ref();
+let ref = firebase.database().ref('users');
 ref.on("value", snap => {
-   let temp = "";
-    var arr = [];
-    var arr2= [];
-    var arr3= [];
-   for(prop in snap.val()){
-       arr.push(snap.val()[prop]["name"]);
-       arr2.push(snap.val()[prop]["leaderbalance"]);
-       arr3.push(prop);
-   }
-    console.log(arr);
-    var len = arr2.length;
-	for (var i = 1; i < len; i++) {
-		var tmp = arr2[i];
-        var tmp2 = arr[i];
-        var tmp3 = arr3[i];
-		for (var j = i - 1; j >= 0 && (arr2[j] > tmp); j--) {
-			//Shift the number
-			arr2[j + 1] = arr2[j];
-            arr[j + 1] = arr[j];
-            arr3[j + 1] = arr3[j];
-		}
-		arr2[j + 1] = tmp;
-        arr[j + 1] = tmp2;
-        arr3[j + 1] = tmp3;
-	}
-    var temparr = [];
-    var temparr2 = [];
-    var temparr3 = [];
-    for(i in arr2){
-        temparr.push(arr2[arr2.length-1-i]);
-    }
+
+    let temp = "";
+    let arr = Object.values(snap.val());
+
+    arr.sort((a, b) => {
+        return b.misses - a.misses;
+    });
+
     for(i in arr){
-        temparr2.push(arr[arr2.length-1-i]);
-    }
-    for(i in arr){
-        temparr3.push(arr3[arr.length-1-i]);
-    }
-    for(i in temparr3){
-        ref.child(temparr3[i]).child("place").set(parseInt(i)+1);
-    }
-    for(i in arr2){
-        temp += '<tr><td>'+temparr2[i]+'</td><td>'+temparr[i]+'</td></tr>';
+        temp += '<tr><td>'+arr[i].name+'</td><td>'+arr[i].misses+'</td></tr>';
 
     }
     document.getElementById('tbody').innerHTML = temp;
