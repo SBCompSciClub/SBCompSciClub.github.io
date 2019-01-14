1e5244046d00e4c9d881d550c09220cb07992a98
Rishav Bhagat <rishavb123@gmail.com>
2019-01-14 18:23:57 -0500

    updated portal

--- portal/events.js
+++ portal/events.js
@@ -1,58 +1,57 @@
-window.addEventListener("_event_onInitializeFirebase", (event) =>
-{
-    firebase.initializeApp(event.detail.configuration);
+let hacksb;
+
+window.addEventListener("_event_onInitializeFirebase", (event) => {
+    firebase.initializeApp(event.detail.configuration[0]);
+    hacksb = firebase.initializeApp(event.detail.configuration[1], "secondary");
     firebase.auth().onAuthStateChanged(event.detail.authChanged);
+    // console.log("BEFORE");
+    // hacksb.database().ref("/").once("value", (e) => {
+    //     console.log("ERROR")
+    //     console.log(e.val());
+    // })
 });
-window.addEventListener("_event_onSetData", (event) =>
-{
-    firebase.database().ref(event.detail.reference).set(event.detail.data);
+window.addEventListener("_event_onSetData", (event) => {
+    ((event.detail.project === "hacksb") ? hacksb : firebase).database().ref(event.detail.reference).set(event.detail.data);
 });
-window.addEventListener("_event_onAppendData", (event) =>
-{
-    firebase.database().ref(event.detail.reference).push().set(event.detail.data, (err) =>
-    {
-        if (err)
-        {
+window.addEventListener("_event_onAppendData", (event) => {
+    ((event.detail.project === "hacksb") ? hacksb : firebase).database().ref(event.detail.reference).push().set(event.detail.data, (err) => {
+        if (err) {
             event.detail.onError();
-        }
-        else
-        {
+        } else {
             event.detail.onComplete();
         }
     });
 });
-window.addEventListener("_event_onUpdateData", (event) =>
-{
-    firebase.database().ref(event.detail.reference).update(event.detail.data);
+window.addEventListener("_event_onUpdateData", (event) => {
+    ((event.detail.project === "hacksb") ? hacksb : firebase).database().ref(event.detail.reference).update(event.detail.data);
 });
-window.addEventListener("_event_onGetData", (event) =>
-{
-    firebase.database().ref(event.detail.reference).once("value", (e) =>
-    {
+window.addEventListener("_event_onGetData", (event) => {
+    ((event.detail.project === "hacksb") ? hacksb : firebase).database().ref(event.detail.reference).once("value", (e) => {
         event.detail.callback(e.val());
     });
 });
-window.addEventListener("_event_onDataChanged", (event) =>
-{
-    firebase.database().ref(event.detail.reference).on("value", (e) =>
-    {
+window.addEventListener("_event_onDataChanged", (event) => {
+    ((event.detail.project === "hacksb") ? hacksb : firebase).database().ref(event.detail.reference).on("value", (e) => {
         event.detail.callback();
     });
 });
-window.addEventListener("_event_onDeleteData", (event) =>
-{
-    firebase.database().ref(event.detail.reference).remove();
+window.addEventListener("_event_onDeleteData", (event) => {
+    ((event.detail.project === "hacksb") ? hacksb : firebase).database().ref(event.detail.reference).remove();
 });
-window.addEventListener("_event_onSignIn", (event) =>
-{
-    firebase.auth().signInWithEmailAndPassword(event.detail.email, event.detail.password).catch((error) =>
-    {
+window.addEventListener("_event_onSignIn", (event) => {
+    firebase.auth().signInWithEmailAndPassword(event.detail.email, event.detail.password).catch((error) => {
+        let errCode = error.code;
+        let errMess = error.message;
+        alert(errMess);
+    });
+
+    hacksb.auth().signInWithEmailAndPassword(event.detail.email, event.detail.password).catch((error) => {
         let errCode = error.code;
         let errMess = error.message;
         alert(errMess);
     });
 });
-window.addEventListener("_event_onSignOut", (event) =>
-{
+window.addEventListener("_event_onSignOut", (event) => {
     firebase.auth().signOut();
+    hacksb.auth().signOut();
 });
